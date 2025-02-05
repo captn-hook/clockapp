247762da66120655f76d5acf5f211664632af467
Tristan H <tristanskyhook@gmail.com>
2025-02-05 12:27:14 -0800

    todos

--- src/basic/button.tsx
+++ src/basic/button.tsx
@@ -10,6 +10,14 @@ const Button = styled.button({
 	color: defaultTheme.colors.white,
 	fontSize: '16px',
 	cursor: 'pointer',
+	outline: 'none',
+	'&:hover': {
+		outline: '1px solid ' + defaultTheme.colors.white,
+	},
+	'&:active': {
+		outline: '2px solid ' + defaultTheme.colors.bluegreen,
+	},
+	transitions: 'all 0.3s',
 })
 
 export default Button
--- src/basic/link.tsx
+++ src/basic/link.tsx
@@ -2,19 +2,27 @@ import styled from '@emotion/styled'
 import { defaultTheme } from '../style'
 
 const LinkStyle = styled.a({
-	color: defaultTheme.colors.white,
-	textDecoration: 'underline',
-	cursor: 'pointer',
-	fontWeight: defaultTheme.fontsizes.bold,
+    color: defaultTheme.colors.white,
+    textDecoration: 'underline',
+    cursor: 'pointer',
+	padding: '5px',
+	borderRadius: '5px',
+    fontWeight: defaultTheme.fontsizes.bold,
+    '&:hover': {
+		backgroundColor: defaultTheme.colors.grey,
+    },
+	'&:active': {
+		backgroundColor: defaultTheme.colors.green,
+	},
 })
 
 // Links are unimplemented now, shows a Coming Soon! popup
 const Link = ({ children, style }: { children: React.ReactNode; style?: React.CSSProperties }) => {
-	return (
-		<LinkStyle style={style} onClick={() => alert('Coming Soon!')}>
-			{children}
-		</LinkStyle>
-	)
+    return (
+        <LinkStyle style={style} onClick={() => alert('Coming Soon!')}>
+            {children}
+        </LinkStyle>
+    )
 }
 
 export default Link
